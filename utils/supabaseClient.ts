679a023de7d3ef2b0ac8c6945977a3c380d73afb
Thiago <thiagolaranjeiramaciel@hotmail.com>
2025-06-17 16:48:18 -0300

    fixing

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const anonKey = process.env.SUPABASE_PUBLIC_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl!, anonKey!);
+export const supabase = createClient(supabaseUrl, anonKey);
