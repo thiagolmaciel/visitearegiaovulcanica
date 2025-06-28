b1f6242428171ff07a76326371bb91b2c1927120
Thiago <thiagolaranjeiramaciel@hotmail.com>
2025-06-28 03:52:54 -0300

    icon

--- lib/iconsMap.tsx
+++ lib/iconsMap.tsx
@@ -3,6 +3,8 @@ import { PiBeerBottleBold, PiCoffeeBeanFill } from "react-icons/pi";
 import { LuCandy, LuGrape } from "react-icons/lu";
 import { HiUserGroup } from "react-icons/hi";
 import { BiSolidCheese, BiSolidCoffee } from "react-icons/bi";
+import { GiHorseHead } from "react-icons/gi";
+
 
 const iconsMap = {
   PiCoffeeBeanFill: PiCoffeeBeanFill,
@@ -15,6 +17,7 @@ const iconsMap = {
   LuCandy: LuCandy,
   BiSolidCheese: BiSolidCheese,
   HiUserGroup: HiUserGroup,
+  GiHorseHead: GiHorseHead,
 };
 
 export default iconsMap;
