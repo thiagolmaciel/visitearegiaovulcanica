e19f1049a1fd17a77c1c50ff5fc02c5c827b3282
Thiago <thiagolaranjeiramaciel@hotmail.com>
2025-08-24 19:24:10 -0300

    fix

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,23 +1,11 @@
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
-import React from 'react'
-import LogoutButton from './components/LogoutButton/page'
-import UsernameComponent from './components/UsernameComponent/page'
-import supabase from '../../../../utils/supabase/client'
+import supabase from "../../../../utils/supabase/client"
+import DashboardForm from "./components/DashboardForm/DashboardForm"
 
-const Page =  async () => {
-  const session = await supabase.auth.getUser()
-  if (!session || session == null) {
-    redirect('/')
-  }
-  console.log(session)
-  return (
-    <div>
-    <div>Olá, <UsernameComponent /></div>
-      <div>dashboard</div>
-      <LogoutButton></LogoutButton>
-    </div>
-  )
+export default async function Account() {
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
+    return <DashboardForm user={user} />
 }
-
-export default Page
